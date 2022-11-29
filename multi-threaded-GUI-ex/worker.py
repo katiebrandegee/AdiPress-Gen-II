2d82fdb71ed2635dcc33dadd4b48e83e6c5d0384
Nick Hinke <nhinke2@gmail.com>
2022-11-28 21:00:53 -0500

    added simple example of multithreaded vs singlethread gui with long-running task

---
+++ multi-threaded-GUI-ex/worker.py
@@ -0,0 +1,18 @@
+import time
+from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
+
+# Step 1: Create a worker class
+class Worker(QObject):
+
+    # define signals (must be defined as class attributes like done here)
+    finished = pyqtSignal()
+    progress = pyqtSignal(int, int)
+
+    @pyqtSlot()
+    def run(self):
+        """Long-running task."""
+        duration = 5
+        for i in range(duration):
+            time.sleep(1)
+            self.progress.emit(i+1, duration)
+        self.finished.emit()
