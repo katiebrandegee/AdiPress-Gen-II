34a022a6884af0827484d0f8c700e65996591906
katiebrandegee <107957881+katiebrandegee@users.noreply.github.com>
2022-12-10 12:51:26 -0500

    Updating states file with sensor input

--- src/states.py
+++ src/states.py
@@ -1,4 +1,17 @@
 from PyQt5.QtCore import QTimer, QObject, pyqtSignal, pyqtSlot
+import RPi.GPIO as GPIO
+import busio
+import board
+import adafruit_ads1x15.ads1115 as ADS 
+from adafruit_ads1x15.analog_in import AnalogIn
+from hx711 import HX711
+import RFID
+import config 
+import pymongo
+import socket
+import datetime
+import sched
+
 
 class State(QObject):
 
@@ -181,6 +194,7 @@ class SampleSetupState(State):
         self._iterateTimer = QTimer(self)
         self._iterateTimer.timeout.connect(self.iterate)
         # TODO define how sensors are read
+        self.plungerSensor, self.compressionDrawerSensor, self.filtrateDrawerSensor, self.nfc = self.setupSampleSetupSensors()
 
         # TODO remove below, just for testing slots in GUI
         self.testPlungerVal = 5000000
@@ -189,6 +203,23 @@ class SampleSetupState(State):
         QTimer.singleShot(6000, lambda: self.testConditions1())
         QTimer.singleShot(8000, lambda: self.testConditions2())
         QTimer.singleShot(10000, lambda: self.testConditions3())
+
+    def setupSampleSetupSensors(self):
+        i2c = busio.I2C(board.SCL, board.SDA)
+        ads5V = ADS.ADS1115(i2c, address=0x49)
+        plungerSensor = AnalogIn(ads5V, ADS.P0)
+        compressionDrawerSensor = AnalogIn(ads5V, ADS.P1)
+        filtrateDrawerSensor = AnalogIn(ads5V, ADS.P2)
+
+        #RFID
+        GPIO.setup(self._consumableRFID_RST, GPIO.OUT)
+        GPIO.setup(self._filtrateRFID_RST, GPIO.OUT)
+        nfc = RFID.NFC()
+        nfc.addBoard("consumableRFID",self._consumableRFID_RST)
+        nfc.addBoard("filtrateRFID",self._filtrateRFID_RST)
+
+        return plungerSensor, compressionDrawerSensor, filtrateDrawerSensor
+
 
     # TODO remove, just for testing slots in GUI
     def testConditions1(self):
@@ -211,10 +242,17 @@ class SampleSetupState(State):
         if (self._configFileSectionName not in parsedConfig):
             raise Exception(f"'{self._configFileSectionName}' section not found in {configFile}...")
         try:
+
             # TODO add any other configurable parameters here
-            self._plungerLimit = int(parsedConfig[self._configFileSectionName]['samplesetupscreenplungerlimit'])
-            self._compressionDrawerLimit = int(parsedConfig[self._configFileSectionName]['samplesetupscreencompressiondrawerlimit'])
-            self._filtrateDrawerLimit = int(parsedConfig[self._configFileSectionName]['samplesetupscreenfiltratedrawerlimit'])
+            self._plungerimit = int(parsedConfig[self._configFileSectionName]['plungersensorlim'])
+            self._compressionDrawerLimit = int(parsedConfig[self._configFileSectionName]['compressiondrawerlim'])
+            self._filtrateDrawerLimit = int(parsedConfig[self._configFileSectionName]['filtratedrawerlimit'])
+            self._consumableRFID_RST = int(parsedConfig[self._configFileSectionName]['consumablerfid_rst'])
+            self._filtrateRFID_RST = int(parsedConfig[self._configFileSectionName]['filtraterfid_rst'])
+            self._consumableGain = int(parsedConfig[self._configFileSectionName]['consumablegain'])
+            self._filtrateGain = int(parsedConfig[self._configFileSectionName]['filtrategain'])
+
+
         except:
             raise Exception(f"All required configurable parameters were not found under the '{self._configFileSectionName}' or 'DEFAULT' sections in {configFile}...")
 
@@ -222,19 +260,19 @@ class SampleSetupState(State):
     def iterate(self):
 
         # read plunger sensor (TODO update with actual sensor reading in if statement, left like this just for testing)
-        plungerSensorValue = self.testPlungerVal
+        plungerSensorValue = self.plungerSensor.value
         if ((plungerSensorValue < self._plungerLimit) != self._plungerCheckPassed):
             self._plungerCheckPassed = (not self._plungerCheckPassed)
             self.plungerStatusChanged.emit("plunger", self._plungerCheckPassed)
 
         # read compression drawer sensor (TODO update with actual sensor reading in if statement, left like this just for testing)
-        compressionDrawerSensorValue = self.testCompressionDrawerVal
+        compressionDrawerSensorValue = self.compressionDrawerSensor.value
         if ((compressionDrawerSensorValue < self._compressionDrawerLimit) != self._compressionDrawerCheckPassed):
             self._compressionDrawerCheckPassed = (not self._compressionDrawerCheckPassed)
             self.compressionDrawerStatusChanged.emit("compressionDrawer", self._compressionDrawerCheckPassed)
 
         # read compression drawer sensor (TODO update with actual sensor reading in if statement, left like this just for testing)
-        filtrateDrawerSensorValue = self.testFiltrateDrawerVal
+        filtrateDrawerSensorValue = self.compressionDrawerSensor.value
         if ((filtrateDrawerSensorValue < self._filtrateDrawerLimit) != self._filtrateDrawerCheckPassed):
             self._filtrateDrawerCheckPassed = (not self._filtrateDrawerCheckPassed)
             self.filtrateDrawerStatusChanged.emit("filtrateDrawer", self._filtrateDrawerCheckPassed)
@@ -243,12 +281,12 @@ class SampleSetupState(State):
 
         # read consumable RFID (TODO update with actual RFID reading in if statement, left like this just for testing)
         if (self._compressionDrawerCheckPassed):
-            self._consumableRFID = 8
+            self._consumableRFID = self.nfc.read("consumableRFID")
 
         # read consumable RFID (TODO update with actual RFID reading in if statement, left like this just for testing)
         if (self._filtrateDrawerCheckPassed):
-            self._filtrateRFID = 9
-
+            self._filtrateRFID = self.nfc.read("filtateRFID")
+        # make sure that nullInt is representative of what you get when theres no card
         allRfIdsRead = (self._consumableRFID != self._nullInt and self._filtrateRFID != self._nullInt)
         if (allRfIdsRead != self._rfidChecksPassed):
             self._rfidChecksPassed = (not self._rfidChecksPassed)
@@ -293,17 +331,52 @@ class CompressionState(State):
 
     goButtonPressedEvent = pyqtSignal()
 
+
     def __init__(self, parsedConfig: dict[str, dict[str, str]], stateMachine, *, parent=None):
         super().__init__(parsedConfig, stateMachine, parent=parent)
         self._goButtonPressed = False
+        self._goButtonPressedFlag = False
+        self._compressionLimitReached = False
         self.readRelevantConfigVars(self._parsedConfig)
         self._iterateTimer = QTimer(self)
         self._iterateTimer.timeout.connect(self.iterate)
+        self.pwmDown, self.pwmUp, self.currentSensor = self.setupCompressionSensors()
         # TODO define how sensors are read
 
     @property
     def name(self):
         return 'Compression'
+
+    def setupCompressionSensors(self):
+        # Actuator Setup
+        GPIO.setup(self._UP_PWM, GPIO.OUT)
+        GPIO.setup(self._DOWN_PWM, GPIO.OUT)
+        GPIO.setup(self._UP_EN, GPIO.OUT)
+        GPIO.setup(self._DOWN_EN, GPIO.OUT)
+
+        GPIO.output(self._UP_EN, GPIO.HIGH)
+        GPIO.output(self._DOWN_EN, GPIO.HIGH)
+
+        # set these better later
+        pwmUp = GPIO.PWM(self._UP_PWM, 50)
+        pwmDown = GPIO.PWM(self._DOWN_PWM, 50)
+
+        pwmUp.start(0)
+        pwmDown.start(0)
+
+        # Current sensor setup
+        i2c = busio.I2C(board.SCL, board.SDA)
+        ads3V = ADS.ADS1115(i2c)
+        currentSensor = AnalogIn(ads3V, ADS.P0)
+
+        # Load Cells
+        loadCell1 = HX711(dout_pin=21, pd_sck_pin=18, channel='A', gain = gain1)
+        loadCell2 = HX711(dout_pin=19, pd_sck_pin=26, channel='A', gain = gain2)
+
+
+        return pwmDown, pwmUp, currentSensor
+
+
 
     def readRelevantConfigVars(self, parsedConfig: dict[str, dict[str, str]]):
         configFile = parsedConfig['CONFIG_FILE_NAME'] if 'CONFIG_FILE_NAME' in parsedConfig else 'config file'
@@ -313,14 +386,30 @@ class CompressionState(State):
             # TODO add any other configurable parameters here
             self._currentLimit = int(parsedConfig[self._configFileSectionName]['compressionscreencurrentlimit'])
             self._dwellDurationSec = float(parsedConfig[self._configFileSectionName]['compressionscreendwelldurationsec'])
+            self._UP_PWM = int(parsedConfig[self._configFileSectionName]['up_pwm'])
+            self._DOWN_PWM = int(parsedConfig[self._configFileSectionName]['down_pwm'])
+            self._UP_EN = int(parsedConfig[self._configFileSectionName]['up_en'])
+            self._DOWN_EN = int(parsedConfig[self._configFileSectionName]['down_en'])
+            self._endPin = int(parsedConfig[self._configFileSectionName]['endpin'])
+            self._homePin = int(parsedConfig[self._configFileSectionName]['homepin'])
+            self._encoder = int(parsedConfig[self._configFileSectionName]['encoder']) 
+            self._consumableGain = int(parsedConfig[self._configFileSectionName]['consumablegain']) 
+            self._filtrateGain = int(parsedConfig[self._configFileSectionName]['filtrategain']) 
+
         except:
             raise Exception(f"All required configurable parameters were not found under the '{self._configFileSectionName}' or 'DEFAULT' sections in {configFile}...")
 
     def iterate(self):
-
+        
         # TODO: check if go button pressed
-        if (self._goButtonPressed):
-            self.goButtonPressedEvent.emit()
+        # check what current sensor reads when nothing is moving
+        # chekc to see if loop executes fast enough for polling go Buttun instead of interrupt
+        if (self._goButtonPressed and not self._compressionLimitReached):
+            self._goButtonPressedFlag = True
+            if (not self._goButtonPressedFlag):
+                self.goButtonPressedEvent.emit()
+
+
 
         # TODO define rest of state (and probably do goButtonStatusChanged for signal instead)
         self._exitCondition = True
